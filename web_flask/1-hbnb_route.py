fb9f9ec9ff1c41f82ff3f5fae9ffd59bc688aa91
jackjavi <jackmtembete@gmail.com>
2023-04-24 02:27:09 +0000

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'."""
     return 'HBNB'
 
 

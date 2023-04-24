b2a827995e79ebf59eaebac497e8109f27c30758
jackjavi <jackmtembete@gmail.com>
2023-04-24 00:55:52 +0000

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,6 +5,7 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
     /hbnb: Display 'HBNB'
+    /c/<text>: Displays 'C' followed by the value of <text>
 """
 from flask import Flask
 
@@ -13,14 +14,17 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'."""
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Displays 'C' followed by the value of <text>."""
     return 'C' + text.replace('_', ' ')
     
 

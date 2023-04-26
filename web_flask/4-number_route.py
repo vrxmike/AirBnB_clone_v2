c00acc22c4cc5a089b02bd0dbb95029520092bc6
vrxmike <alvinvrxmike@icloud.com>
2023-04-26 16:59:20 -0700

    4-Task-Fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'."""
-    returun "Hello HBNB!"
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
